3a778ae36599127e5ed6a4a68d218e7ac06b6235
joannenea <cpccbaby@gmail.com>
2018-11-22 00:35:47 +0800

    Update fork.cpp

--- fork.cpp
+++ fork.cpp
@@ -1,158 +1,175 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+#include <fcntl.h>
+#include <signal.h>
 #include <sys/types.h>
 #include <sys/socket.h>
-#include <sys/wait.h>
+#include <netinet/in.h>
 #include <arpa/inet.h>
-#include <unistd.h>
-#include <stdlib.h>
-#include <stdio.h>
-#include <string.h>
-#include <fcntl.h> 
-#include <errno.h> 
-#include <string>
-#include <stdexcept>
-#include <iostream>
-#include <sys/ioctl.h>
-#define SERVER_PORT 8888 
-#define BUFFER_LENGTH 1024
-using namespace std;
-class SocketException : public std::logic_error
+
+#define BUFSIZE 8096
+
+struct {
+    char *ext;
+    char *filetype;
+} extensions [] = {
+    {"gif", "image/gif" },
+    {"jpg", "image/jpeg"},
+    {"jpeg","image/jpeg"},
+    {"png", "image/png" },
+    {"zip", "image/zip" },
+    {"gz",  "image/gz"  },
+    {"tar", "image/tar" },
+    {"htm", "text/html" },
+    {"html","text/html" },
+    {"exe","text/plain" },
+    {0,0} };
+
+void handle_socket(int fd)
+
+BloggerAds 部落格行銷
+
+BloggerAds 部落格行銷
+
+BloggerAds 部落格行銷
 {
-public:
-SocketException(const std::string& what) throw ()
-: std::logic_error(what) {};
-};
-int SocketRead(int& fd, std::string& strValue)
-{
-// 每次只读取一个字节
-int nRead = 1;
-auto_ptr<char> apBuf(new char[nRead]);
-int r = read(fd, apBuf.get(), nRead) ;
-if (0 < r)
-{
-   strValue.assign(apBuf.get(), r);
-}
-return r;
-}
-int SocketWrite(int& fd, const char* pBuf, int nBufLen)
-{
-int nWrite = 0;
-int nError = -1;
-/*
-SIGPIPE：sent to a process when it attempts to write to a pipe without a process connected to the other end. The symbolic constant for SIGPIPE is defined in the header file signal.h. Symbolic signal names are used because signal numbers can vary across platforms.
-*/
-// SIG_IGN:忽略信号
-sighandler_t sh = signal(SIGPIPE, SIG_IGN);
-while (nWrite < nBufLen)
-{
-   nError = send(fd, pBuf + nWrite, nBufLen - nWrite, 0); 
-   if (0 > nError)
-   {
-    perror(strerror(errno));
-    // 见文末
-    if (nError == EINTR) continue;
-    if (nError == EPIPE) 
-    {
-     close(fd); 
+    int j, file_fd, buflen, len;
+    long i, ret;
+    char * fstr;
+    static char buffer[BUFSIZE+1];
+
+    ret = read(fd,buffer,BUFSIZE);   /* 讀取瀏覽器要求 */
+    if (ret==0||ret==-1) {
+     /* 網路連線有問題，所以結束行程 */
+        exit(3);
     }
-    // 恢复信号
-    signal(SIGPIPE, sh);
-    throw SocketException(string("SocketWrite error, ") + strerror(errno));
-   }
-   else if (0 == nError)
-   {
-    signal(SIGPIPE, sh);
-    throw SocketException(string("SocketWrite error, ") + strerror(errno));
-   }
-   else
-   {
-    nWrite += nError;
-   }
-}
-signal(SIGPIPE, sh);
-return nError;
-}
-void SetOption(int s, int level, int optname, const void *optval, socklen_t optlen)
-{
-if (setsockopt(s, level, optname, optval, optlen) < 0)
-{
-   perror(strerror(errno));
-}
-}
-int main(int argc, char*argv[])
-{
-int fdServer = -1;
-int   fdClient = -1;
-int   nStatus = -1;
-pid_t pid = 0;
-int nSockAddrLen = sizeof(struct sockaddr_in);
-struct sockaddr_in addrServer;
-struct sockaddr_in addrSocket;
-bzero(&addrServer, sizeof(struct sockaddr_in)); 
-bzero(&addrSocket, sizeof(struct sockaddr_in));
-if(-1 == (fdServer = socket(AF_INET, SOCK_STREAM, 0)))
-{
-   perror(strerror(errno));
-   exit(-1);
-}
-int nReuseAddr = 1;
-SetOption(fdServer, SOL_SOCKET, SO_REUSEADDR, &nReuseAddr, sizeof(int));
-addrServer.sin_family = AF_INET;
-addrServer.sin_port = htons(SERVER_PORT);
-addrServer.sin_addr.s_addr = INADDR_ANY;
-if (-1 == bind(fdServer, (struct sockaddr *)&addrServer, sizeof(struct sockaddr))) 
-{ 
-   perror(strerror(errno));
-   exit(-1);
-}
-if (-1 == listen(fdServer, 128))
-{
-   perror(strerror(errno));
-   exit(-1);
+
+    /* 程式技巧：在讀取到的字串結尾補空字元，方便後續程式判斷結尾 */
+    if (ret>0&&ret<BUFSIZE)
+        buffer[ret] = 0;
+    else
+        buffer[0] = 0;
+
+    /* 移除換行字元 */
+    for (i=0;i<ret;i++) 
+        if (buffer[i]=='\r'||buffer[i]=='\n')
+            buffer[i] = 0;
+    
+    /* 只接受 GET 命令要求 */
+    if (strncmp(buffer,"GET ",4)&&strncmp(buffer,"get ",4))
+        exit(3);
+    
+    /* 我們要把 GET /index.html HTTP/1.0 後面的 HTTP/1.0 用空字元隔開 */
+    for(i=4;i<BUFSIZE;i++) {
+        if(buffer[i] == ' ') {
+            buffer[i] = 0;
+            break;
+
+BloggerAds 部落格行銷
+        }
+    }
+
+    /* 檔掉回上層目錄的路徑『..』 */
+    for (j=0;j<i-1;j++)
+        if (buffer[j]=='.'&&buffer[j+1]=='.')
+            exit(3);
+
+    /* 當客戶端要求根目錄時讀取 index.html */
+    if (!strncmp(&buffer[0],"GET /\0",6)||!strncmp(&buffer[0],"get /\0",6) )
+        strcpy(buffer,"GET /index.html\0");
+
+    /* 檢查客戶端所要求的檔案格式 */
+    buflen = strlen(buffer);
+    fstr = (char *)0;
+
+    for(i=0;extensions[i].ext!=0;i++) {
+        len = strlen(extensions[i].ext);
+        if(!strncmp(&buffer[buflen-len], extensions[i].ext, len)) {
+            fstr = extensions[i].filetype;
+            break;
+        }
+    }
+
+    /* 檔案格式不支援 */
+    if(fstr == 0) {
+        fstr = extensions[i-1].filetype;
+    }
+
+    /* 開啟檔案 */
+    if((file_fd=open(&buffer[5],O_RDONLY))==-1)
+  write(fd, "Failed to open file", 19);
+
+    /* 傳回瀏覽器成功碼 200 和內容的格式 */
+    sprintf(buffer,"HTTP/1.0 200 OK\r\nContent-Type: %s\r\n\r\n", fstr);
+    write(fd,buffer,strlen(buffer));
+
+
+    /* 讀取檔案內容輸出到客戶端瀏覽器 */
+    while ((ret=read(file_fd, buffer, BUFSIZE))>0) {
+        write(fd,buffer,ret);
+    }
+
+    exit(1);
 }
 
-while (true)
+main(int argc, char **argv)
 {
-   if ((fdClient = accept(fdServer, (struct sockaddr *)&addrSocket, (socklen_t*)&nSockAddrLen)) < 0)
-   {
-    perror(strerror(errno));
-    exit(-1);
-   }
-   printf("client address:%s\t port:%d\r\n", inet_ntoa(addrSocket.sin_addr), ntohs(addrSocket.sin_port));
-   if ((pid = fork()) < 0)
-   {
-    perror(strerror(errno));
-    exit(-1);
-   }
-   else if (0 == pid) /* clild */
-   {
-    while (true)
-    {
-     try
-     {
-      std::string strValue;
-      if(0 < SocketRead(fdClient, strValue))
-      {
-       SocketWrite(fdClient, strValue.c_str(), strValue.length());
-      }
-      else
-      {
-       close(fdClient); 
-      }
-      // 要有延时，不然CPU使用率很高
-      usleep(10);
-     }
-     catch(const SocketException& e)
-     {
-      cerr << e.what() << endl; 
-      close(fdClient);
-      break;
-     }
+    int i, pid, listenfd, socketfd;
+    size_t length;
+    static struct sockaddr_in cli_addr;
+    static struct sockaddr_in serv_addr;
+
+    /* 使用 /tmp 當網站根目錄 */
+    if(chdir("/tmp") == -1){ 
+        printf("ERROR: Can't Change to directory %s\n",argv[2]);
+        exit(4);
     }
-   }
-   else /* parent */
-   {
-    close(fdClient); 
-   }
-}
-return 0;
+
+    /* 背景繼續執行 */
+    if(fork() != 0)
+        return 0;
+
+    /* 讓父行程不必等待子行程結束 */
+    signal(SIGCLD, SIG_IGN);
+
+    /* 開啟網路 Socket */
+    if ((listenfd=socket(AF_INET, SOCK_STREAM,0))<0)
+        exit(3);
+
+    /* 網路連線設定 */
+    serv_addr.sin_family = AF_INET;
+    /* 使用任何在本機的對外 IP */
+    serv_addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    /* 使用 2333 Port */
+    serv_addr.sin_port = htons(2333);
+
+    /* 開啟網路監聽器 */
+    if (bind(listenfd, (struct sockaddr *)&serv_addr,sizeof(serv_addr))<0)
+        exit(3);
+
+    /* 開始監聽網路 */
+    if (listen(listenfd,64)<0)
+        exit(3);
+
+    while(1) {
+        length = sizeof(cli_addr);
+        /* 等待客戶端連線 */
+        if ((socketfd = accept(listenfd, (struct sockaddr *)&cli_addr, &length))<0)
+            exit(3);
+
+        /* 分出子行程處理要求 */
+        if ((pid = fork()) < 0) {
+            exit(3);
+        } else {
+            if (pid == 0) {  /* 子行程 */
+                close(listenfd);
+                handle_socket(socketfd);
+            } else { /* 父行程 */
+                close(socketfd);
+            }
+        }
+    }
 }
